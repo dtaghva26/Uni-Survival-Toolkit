193fb562276f9516ef03110ff45ccf1dfab787e4
VoidLog Dev <dtaghva26@gmail.com>
2026-03-25 18:36:52 +0000

    fix-automation fix-automation

---
+++ create-commit.bat
@@ -0,0 +1,22 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Get full message
+set msg=%*
+set msg=%msg:"=%
+
+if "%msg%"=="" (
+    echo ❌ Provide a message
+    exit /b
+)
+
+:: Build feature name from message
+set feature=%msg%
+set feature=!feature: =-!
+set feature=!feature::=-!
+set feature=!feature:,=-!
+set feature=!feature:.=-!
+set feature=!feature:/=-!
+
+:: Call main script
+runtime-envs\commit.bat !feature! "%msg%"
